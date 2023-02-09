3d26da17cfa4aaec88dc144e82a0b2ca8c00fc78
CatUnderNewTent <kviatkovsky21@gmail.com>
2023-02-10 01:54:10 +0400

    adding html and css to card component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faMinus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-
+  faTimes = faTimes;
+  faPlus = faPlus;
+  faMinus = faMinus;
 }
